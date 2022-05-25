b5e71670d25ea0509f18d7c0637df3133fe17aba
daft2 <daftdevs@gmail.com>
2022-05-25 13:50:16 +0700

    feat: create whitelist smart contract

---
+++ hardhat/contracts/Whitelist.sol
@@ -0,0 +1,44 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+    //max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
+
+    //create mapping of whitelistedAddresses
+    //if address is whitelisted it would set to true, its false by default for all other address
+    mapping(address => bool) public whitelistedAddresses;
+
+    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
+    uint8 public numAddressesWhitelisted;
+
+    // Setting the Max number of whitelisted addresses
+    // User will put the value at the time of deployment
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    /**
+    addAddressToWhitelist - This function adds the address of the sender to the
+    whitelist
+    */
+
+    function addAddressToWhitelist() public {
+        // check if the user has already been whitelisted
+        require(
+            !whitelistedAddresses[msg.sender],
+            "Sender has already been whitelisted"
+        );
+        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+        require(
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "More addresses cant be added, limit reached"
+        );
+
+        // Add the address which called the function to the whitelistedAddress array
+        whitelistedAddresses[msg.sender] = true;
+
+        // Increase the number of whitelisted addresses
+        numAddressesWhitelisted += 1;
+    }
+}
